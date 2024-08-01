fbb11c6ac43d34e10731da433f05230dec68c4c0
Xenona <xenona11x@gmail.com>
2024-08-01 13:05:56 +0300

    Implemented all necessary styles

---
+++ src/components/BookmarkButton/styled.ts
@@ -0,0 +1,20 @@
+import styled from 'styled-components';
+
+export const MainButton = styled.button`
+  width: 59px;
+  height: 59px;
+  background-color: var(--button-color);
+  border-radius: 100%;
+`;
+
+export const ProfileButton = styled.button`
+  width: 59px;
+  height: 59px;
+  background-color: var(--surface-color);
+  border-radius: 100%;
+`;
+
+export const ButtonIcon = styled.img`
+  margin: auto;
+  display: block;
+`;
