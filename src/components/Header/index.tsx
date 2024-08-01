8304a41e21ea5764c32d4c70ef4c3ed950628c75
Xenona <xenona11x@gmail.com>
2024-08-01 21:01:48 +0300

    Implemented footer

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,25 +1,27 @@
-import museumLogo from '@assets/logos/museum_logo.svg';
 import menu from '@assets/icons/menu.svg';
 import { useState } from 'react';
 import { Menu } from './Menu';
+import museumLogo from '@assets/logos/museum_logo.svg';
 import {
   DesktopMenu,
   Header,
-  Logo,
   MobileMenu,
   MobileMenuButton,
   TopLine,
 } from './styled';
-
+import { LinkedLogo } from './LinkedLogo';
 export function AppHeader() {
   const [isMenuOpened, setIsMenuOpened] = useState<boolean>(false);
 
   return (
     <Header>
       <TopLine>
-        <Logo
-          src={museumLogo}
-          alt="Logo of the museum, depicts an Ancient Greek temple facade and words - Museum of Art"
+        <LinkedLogo
+          img={museumLogo}
+          link={'/'}
+          alt={
+            'Logo of the museum, depicts an Ancient Greek temple facade and words - Museum of Art'
+          }
         />
         <MobileMenuButton onClick={() => setIsMenuOpened(!isMenuOpened)}>
           <img src={menu} alt="Menu icon" />
