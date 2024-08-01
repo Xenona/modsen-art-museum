b3abc0158837afaac02f8659240e167f7d645117
Xenona <xenona11x@gmail.com>
2024-08-01 09:23:14 +0300

    Implemented decorated h1 header

---
+++ src/components/HeadLine/styled.ts
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+export const StyledH1 = styled.h1`
+  font-weight: 700;
+  font-size: 64px;
+  color: var(--regular-text-color);
+  text-align: center;
+
+  span {
+    color: var(--strong-accent-color);
+  }
+`;
