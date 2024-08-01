091ffec4eb79b7c02646839676def4bb116392b7
Xenona <xenona11x@gmail.com>
2024-08-01 23:01:28 +0300

    Implemented Fav page

--- src/pages/favourites/index.tsx
+++ src/pages/favourites/index.tsx
@@ -1,3 +1,27 @@
+import { StyledH1 } from '@components/HeadLine/styled';
+import { SectionHeader } from '@components/SectionHeader';
+import bookmarkIcom from '@assets/icons/bookmark_bright.svg';
+import { BigIcon } from './styled';
+import { Main } from '@pages/home/styled';
+import { ShortGallery } from '@components/ShortGallery';
+
 export function FavouritesPage() {
-  return <>Favourites Page</>;
+  return (
+    <Main>
+      <StyledH1>
+        Here Are Your
+        <br />
+        <BigIcon src={bookmarkIcom} alt="Yellow bookmark icon" />
+        <span>Favourites</span>
+      </StyledH1>
+
+      <section>
+        <SectionHeader
+          topText={'Saved by you'}
+          bottomText={'Your favorites list'}
+        />
+        <ShortGallery />
+      </section>
+    </Main>
+  );
 }
---
+++ src/pages/favourites/styled.ts
@@ -0,0 +1,6 @@
+import styled from 'styled-components';
+
+export const BigIcon = styled.img`
+  height: 74px;
+  vertical-align: middle;
+`;
