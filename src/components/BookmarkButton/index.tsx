83aa3300ddbfed212c9377138493990a1c36c4ff
Xenona <xenona11x@gmail.com>
2024-08-01 12:54:42 +0300

    Implemented all necessary functionality

--- src/components/BookmarkButton/index.tsx
+++ src/components/BookmarkButton/index.tsx
@@ -1,14 +1,26 @@
 import './styles.css';
 import bookmarkIcon from '@assets/icons/bookmark.svg';
 import bookmarkFilledIcon from '@assets/icons/bookmark_filled.svg';
+import { useFavStorageContext } from '@components/FavStorageProvider';
 import { useState } from 'react';
 
-export function BookmarkButton() {
-  // TODO: attach localstorage
-  const [isSavedToFav] = useState<boolean>(false);
+export function BookmarkButton(id: number) {
+  const favStorageContext = useFavStorageContext();
+  const [isSavedToFav, setIsSavedToFav] = useState<boolean>(
+    favStorageContext.check(id),
+  );
+
+  const handleClick = (id: number) => {
+    setIsSavedToFav(!isSavedToFav);
+    if (isSavedToFav) {
+      favStorageContext.save(id);
+    } else {
+      favStorageContext.remove(id);
+    }
+  };
 
   return (
-    <button>
+    <button onClick={() => handleClick(id)}>
       {isSavedToFav ? (
         <img
           src={bookmarkFilledIcon}
