8056721c17a4bb5589042fd9d8a57d228105a585
Xenona <xenona11x@gmail.com>
2024-08-01 13:09:54 +0300

    Reused styles for h4

--- src/components/SectionHeader/styles.ts
+++ src/components/SectionHeader/styles.ts
@@ -4,12 +4,6 @@ export const HeaderGroup = styled.hgroup`
   text-align: center;
 `;
 
-export const TopText = styled.h4`
-  font-weight: 400;
-  font-size: 16px;
-  color: var(--weak-accent-color);
-`;
-
 export const BottomText = styled.h2`
   font-weight: 400;
   font-size: 32px;
