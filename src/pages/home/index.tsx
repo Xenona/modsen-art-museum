7ada26810327053278ecc37c9a46d6a828e66ef1
Xenona <xenona11x@gmail.com>
2024-08-01 10:23:13 +0300

    Added section header

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,5 +1,6 @@
 import { HeadLine } from '@components/HeadLine';
 import { SearchInput } from '@components/SearchInput';
+import { SectionHeader } from '@components/SectionHeader';
 
 export function HomePage() {
   return (
@@ -8,6 +9,12 @@ export function HomePage() {
         Let's Find <span>Some</span> Art <br /> Here!
       </HeadLine>
       <SearchInput />
+      <section>
+        <SectionHeader
+          topText="Topics for you"
+          bottomText="Our special gallery"
+        />
+      </section>
     </>
   );
 }
