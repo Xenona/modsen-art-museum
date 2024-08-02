e6b7ba6728216f540563521f73e7aea78ed3af4b
Xenona <xenona11x@gmail.com>
2024-08-02 21:23:24 +0300

    Implemented StubImage

--- src/components/InfoCard/index.tsx
+++ src/components/InfoCard/index.tsx
@@ -1,4 +1,4 @@
-import { BookmarkButton } from '@components/BookmarkButton';
+import { BookmarkButton } from "@components/BookmarkButton";
 import {
   Author,
   Card,
@@ -6,10 +6,11 @@ import {
   PublicDomain,
   Thumbnail,
   Title,
-} from './styled';
-import { Link } from 'react-router-dom';
-import { Art } from '@components/SpecialGallery';
-import { IMAGE_LOWQ_ENDPOINT } from '@constants/api';
+} from "./styled";
+import { Link } from "react-router-dom";
+import { Art } from "@components/SpecialGallery";
+import { IMAGE_LOWQ_ENDPOINT } from "@constants/api";
+import { StubImage } from "@components/StubImage";
 
 export function InfoCard({
   data,
@@ -19,19 +20,23 @@ export function InfoCard({
   withImage: boolean;
 }) {
   return (
-    <Link to={`/artwork/${data.id}`}>
+    <Link to={`/artwork/${data.id}`} state={data}>
       <Card $withImage={withImage}>
-        {withImage && (
-          <Thumbnail
-            src={IMAGE_LOWQ_ENDPOINT(data.image_id)}
-            alt={data.thumbnail?.alt_text ?? ''}
-          />
-        )}
+        {withImage ? (
+          data.image_id ? (
+            <Thumbnail
+              src={IMAGE_LOWQ_ENDPOINT(data.image_id)}
+              alt={data.thumbnail?.alt_text ?? ""}
+            />
+          ) : (
+            <StubImage />
+          )
+        ) : null}
         <Content>
           <Title>{data.title}</Title>
           <Author>{data.artist_title}</Author>
           <PublicDomain>
-            {data.on_loan_display ? 'Public' : 'Private'}
+            {data.on_loan_display ? "Public" : "Private"}
           </PublicDomain>
         </Content>
         <BookmarkButton id={data.id} />
