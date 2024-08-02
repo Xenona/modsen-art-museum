e43fe114d13dbdf64e9687d666e23059597a4919
Xenona <xenona11x@gmail.com>
2024-08-02 13:37:07 +0300

    Implemented 404 page
    * also fixed footer fixed position.

--- src/components/Footer/styled.ts
+++ src/components/Footer/styled.ts
@@ -8,4 +8,10 @@ export const Footer = styled.footer`
   padding: 32px 8%;
   background-color: var(--surface-color);
   margin-top: 120px;
+
+  position: fixed;
+  width: 100%;
+  left: 0;
+  bottom: 0;
+  box-sizing: border-box;
 `;
