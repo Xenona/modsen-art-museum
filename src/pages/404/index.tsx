bcc9cdcb63d237c5866be933fd1da197d2ebc0de
Xenona <xenona11x@gmail.com>
2024-08-02 13:37:07 +0300

    Implemented 404 page
    * also fixed footer fixed position.

--- src/pages/404/index.tsx
+++ src/pages/404/index.tsx
@@ -1,3 +1,12 @@
+import { Main } from '@pages/home/styled';
+import { HighH1 } from './styled';
+
 export function NotFound() {
-  return <>404 - not found</>;
+  return (
+    <Main>
+      <HighH1>
+        <span>404</span> — page was not found!
+      </HighH1>
+    </Main>
+  );
 }
