3e4c987fc3fc3a8ec2df9f6246832e31170611c5
Xenona <xenona11x@gmail.com>
2024-08-03 14:23:02 +0300

    Added random gallery

--- src/components/ShortGallery/index.tsx
+++ src/components/ShortGallery/index.tsx
@@ -1,33 +1,13 @@
-import { InfoCard } from '@components/InfoCard';
-import './styles.css';
-import { IData } from '@components/ArtworkCard';
+import { InfoCard } from "@components/InfoCard";
+import { Art } from "@utils/schema";
+import { ShortGalleryContainer } from "./styled";
 
-export function ShortGallery() {
-  // TODO fetch random 12 works
-  const data: IData = {
-    imgSrc:
-      'https://www.artic.edu/iiif/2/21cfd347-e6aa-6a11-f1e7-bf84a4e87a3d/full/843,/0/default.jpg',
-    title: 'The Beach at Sainte-Adresse',
-    artist: 'Claude Monet',
-    isPublicDomain: true,
-    altText:
-      'Painting of a beach on a cloudy day with several sailboats on the water, a few rowboats on the shore, and two groupings of people on the sand.',
-    id: 1,
-  };
+export function ShortGallery({ artworks }: { artworks: Art[] }) {
   return (
-    <div className="shortGal">
-      <InfoCard data={data} withImage={true} />
-      <InfoCard data={data} withImage={true} />
-      <InfoCard data={data} withImage={true} />
-      <InfoCard data={data} withImage={true} />
-      <InfoCard data={data} withImage={true} />
-      <InfoCard data={data} withImage={true} />
-      <InfoCard data={data} withImage={true} />
-      <InfoCard data={data} withImage={true} />
-      <InfoCard data={data} withImage={true} />
-      <InfoCard data={data} withImage={true} />
-      <InfoCard data={data} withImage={true} />
-      <InfoCard data={data} withImage={true} />
-    </div>
+    <ShortGalleryContainer>
+      {artworks.map((art) => (
+        <InfoCard data={art} withImage={true} key={art.id} />
+      ))}
+    </ShortGalleryContainer>
   );
 }
