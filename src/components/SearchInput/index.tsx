8f4c57f6e084b24728cf44107e4d0dd917b1c641
Xenona <xenona11x@gmail.com>
2024-08-03 19:14:34 +0300

    Search implemnted

--- src/components/SearchInput/index.tsx
+++ src/components/SearchInput/index.tsx
@@ -1,38 +1,61 @@
 import { useSearchParams } from "react-router-dom";
 import { SearchContainer, SearchInput as Input, SearchIcon } from "./styled";
-
-const validateQuery = (query: string) => {
-  const maxLength = 100;
-  const trimmedQuery = query.trim();
-  if (trimmedQuery.length === 0 || trimmedQuery.length > maxLength) {
-    return "";
-  }
-  return trimmedQuery;
-};
+import { ApiController } from "@utils/ApiController";
+import { useDebounce } from "@utils/useDebounce";
+import { useEffect, useState } from "react";
+import { useQuery } from "@tanstack/react-query";
+import { ShortGallery } from "@components/ShortGallery";
+import { ApiError } from "@utils/ApiError";
+import { ShortGallerySkeleton } from "@components/skeletons/ShortGallerySkeleton";
+import { ServerError } from "@pages/500";
+import { BottomText } from "@components/SectionHeader/styles";
 
 export function SearchInput() {
   const [searchParams, setSearchParams] = useSearchParams();
+  const [query, setQuery] = useState(searchParams.get("search") || "");
+  const debouncedQuery = useDebounce(query);
 
-  function handleSearch(query: string) {
-    if (query) {
-      searchParams.set('search', query);
+  useEffect(() => {
+    if (debouncedQuery) {
+      searchParams.set("search", debouncedQuery);
     } else {
-      searchParams.delete('search');
+      searchParams.delete("search");
     }
     setSearchParams(searchParams);
-    // TODO: finish searching logic
-  }
+    console.log(
+      ApiController.getSearch({ q: searchParams.get("search") as string }),
+    );
+  }, [debouncedQuery]);
+
+  const { data, error, isPending } = useQuery({
+    queryKey: ["search", debouncedQuery],
+    queryFn: () => ApiController.getSearch({ q: debouncedQuery }),
+  });
+
+  if (error) console.error("LOL", error);
 
   return (
-    <SearchContainer>
-      <Input
-        id="searchbar"
-        type="text"
-        placeholder="Search Art, Artist, Work..."
-        onChange={(e) => handleSearch(e.target.value)}
-        defaultValue={searchParams.get('search')?.toString() || ''}
-      />
-      <SearchIcon />
-    </SearchContainer>
+    <>
+      <SearchContainer>
+        <Input
+          id="searchbar"
+          type="text"
+          placeholder="Search Art, Artist, Work..."
+          onChange={(e) => setQuery(e.target.value)}
+          value={query}
+          defaultValue={searchParams.get("search")?.toString() || ""}
+        />
+        <SearchIcon />
+      </SearchContainer>
+      {isPending ? (
+        <ShortGallerySkeleton />
+      ) : data instanceof ApiError ? (
+        <ServerError />
+      ) : data && data.length ? (
+        <ShortGallery artworks={data} />
+      ) : (
+        <BottomText>nothing was found!</BottomText>
+      )}
+    </>
   );
 }
--- src/utils/ApiController.ts
+++ src/utils/ApiController.ts
@@ -4,6 +4,7 @@ import {
   artAndPaginationSchema,
   artObjectSchema,
   paginationObjectSchema,
+  searchAndPaginationSchema,
 } from "./schema";
 import { ApiError } from "./ApiError";
 
@@ -79,6 +80,29 @@ export class ApiController {
       console.error("Some artworks could not be fetched:", errors);
     }
 
+    return artworks;
+  }
+
+  public static async getSearch({
+    q,
+    size = 12,
+  }: {
+    q: string;
+    size?: number;
+  }): Promise<Art[] | ApiError> {
+    const response = await fetch(
+      `${ARTWORKS_ENDPOINT}/search?q=${q}&size=${size}`,
+    );
+    if (response.status === 404)
+      return new ApiError(404, "Artworks can not be found");
+    if (!response.ok)
+      return new ApiError(response.status, "Could not fetch the artwork");
+    const data = await response.json();
+    const searchAndPagination = searchAndPaginationSchema.safeParse(data);
+    if (!searchAndPagination.success)
+      return new ApiError(422, "Retrieved data is in wrong format");
+    const artIds = searchAndPagination.data.data;
+    const artworks = await this.getArtworks(artIds.map((id) => id.id));
     return artworks;
   }
 }
