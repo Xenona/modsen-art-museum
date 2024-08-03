712bcd0b0e9a2ff89a42030ef9b8c6c631b29524
Xenona <xenona11x@gmail.com>
2024-08-03 16:56:03 +0300

    Implemented paginator skeleton

---
+++ src/components/skeletons/PaginatorSkeleton.styled.ts
@@ -0,0 +1,21 @@
+import styled from "styled-components";
+import { blinkAnimation } from "./ShortGallerySkeleton.styled";
+
+export const SelectorContainerSkeleton = styled.div`
+  display: flex;
+  gap: 10px;
+  justify-content: flex-end;
+  align-items: center;
+  margin-top: 90px;
+  margin-bottom: 32px;
+  width: fit-content;
+  margin-left: auto;
+  animation: ${blinkAnimation} 1.5s infinite;
+`;
+
+export const SelectorButtonSkeleton = styled.div`
+  height: 30px;
+  width: 30px;
+  border-radius: 4px;
+  background-color: #f0f1f1;
+`;
