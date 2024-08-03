6c1a1a82f823cfe49242cc252a5a20cf29a36956
Xenona <xenona11x@gmail.com>
2024-08-04 00:59:02 +0300

    Added tests for local storage

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -8,37 +8,45 @@ import { FlatCompat } from "@eslint/eslintrc";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const compat = new FlatCompat({
-    baseDirectory: __dirname,
-    recommendedConfig: js.configs.recommended,
-    allConfig: js.configs.all
+  baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
+  allConfig: js.configs.all,
 });
 
-export default [...compat.extends(
+export default [
+  ...compat.extends(
     "eslint:recommended",
     "plugin:react/recommended",
     "plugin:@typescript-eslint/recommended",
     "prettier",
-), {
+  ),
+  {
+    ignores: ["*.test.tsx", "*.test.ts"],
+  },
+  {
     plugins: {
-        prettier,
+      prettier,
     },
 
     languageOptions: {
-        globals: {
-            ...globals.browser,
-            ...globals.node
-        },
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+      },
     },
 
     settings: {
-        react: {
-            version: "detect",
-        },
+      react: {
+        version: "detect",
+      },
     },
 
     rules: {
-        "prettier/prettier": "error",
-        "react/react-in-jsx-scope": "off",
-        "react/no-unescaped-entities": "off"
+      "prettier/prettier": "error",
+      "react/react-in-jsx-scope": "off",
+      "react/no-unescaped-entities": "off",
+      "@typescript-eslint/no-loss-of-precision": 'off'
+
     },
-}];
+  },
+];
