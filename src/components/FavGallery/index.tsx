c6e7eee7c449e92e4ae5029f285617740d419808
Xenona <xenona11x@gmail.com>
2024-08-03 16:06:47 +0300

    Added favs

---
+++ src/components/FavGallery/index.tsx
@@ -0,0 +1,34 @@
+import { useFavStorageContext } from "@components/FavStorageProvider";
+import { SectionHeader } from "@components/SectionHeader";
+import { BottomText } from "@components/SectionHeader/styles";
+import { ShortGallery } from "@components/ShortGallery";
+import { useSuspenseQuery } from "@tanstack/react-query";
+import { ApiController } from "@utils/ApiController";
+export function FavGallery() {
+  const favStorageContext = useFavStorageContext();
+  const ids = favStorageContext.getAll();
+
+  const { data: artworks, error } = useSuspenseQuery({
+    queryKey: ["fav", ids],
+    queryFn: () => ApiController.getArtworks(ids),
+  });
+
+  if (error) throw error;
+
+  return (
+    <section>
+      <SectionHeader
+        topText={"Saved by you"}
+        bottomText={"Your favorites list"}
+      />
+      {artworks.length ? (
+        <ShortGallery artworks={artworks} />
+      ) : (
+        <BottomText>
+          Nothing in your personal gallery yet. Try clicking on the bookmark
+          button on artworks!
+        </BottomText>
+      )}
+    </section>
+  );
+}
