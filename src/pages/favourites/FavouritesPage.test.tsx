a80a905d8066bd71a60cab3df1d34228e46d6d24
Xenona <xenona11x@gmail.com>
2024-08-04 18:46:51 +0300

    Added tests for favourites page

---
+++ src/pages/favourites/FavouritesPage.test.tsx
@@ -0,0 +1,54 @@
+import React from "react";
+global.React = React;
+import { render, screen, within } from "@testing-library/react";
+import "@testing-library/jest-dom";
+import { FavouritesPage } from ".";
+
+jest.mock("@components/FavGallery", () => ({
+  FavGallery: () => <div data-testid="fav-gallery">FavGallery</div>,
+}));
+jest.mock("@components/SectionHeader", () => ({
+  SectionHeader: ({
+    topText,
+    bottomText,
+  }: {
+    topText: string;
+    bottomText: string;
+  }) => (
+    <div data-testid="section-header">
+      <div>{topText}</div>
+      <div>{bottomText}</div>
+    </div>
+  ),
+}));
+jest.mock("@components/skeletons/ShortGallerySkeleton", () => ({
+  ShortGallerySkeleton: () => (
+    <div data-testid="short-gallery-skeleton">Loading...</div>
+  ),
+}));
+jest.mock("@components/ErrorBoundary", () => ({
+  ErrorBoundary: ({ children }: { children: React.ReactNode }) => (
+    <div data-testid="error-boundary">{children}</div>
+  ),
+}));
+
+describe("FavouritesPage", () => {
+  it("renders correctly", async () => {
+    render(<FavouritesPage />);
+
+    const heading = screen.getByRole("heading", { level: 1 });
+    expect(within(heading).getByText("Here Are Your")).toBeInTheDocument();
+    expect(
+      within(heading).getByAltText("Yellow bookmark icon"),
+    ).toBeInTheDocument();
+    expect(within(heading).getByText("Favourites")).toBeInTheDocument();
+
+    expect(screen.getByTestId("section-header")).toBeInTheDocument();
+    expect(screen.getByText("Saved by you")).toBeInTheDocument();
+    expect(screen.getByText("Your favorites list")).toBeInTheDocument();
+
+    expect(screen.getByTestId("error-boundary")).toBeInTheDocument();
+
+    expect(screen.getByTestId("fav-gallery")).toBeInTheDocument();
+  });
+});
