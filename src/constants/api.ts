0b0dd3a2c956b5a3d8cc5fc88f7bab2f033a0ed7
Xenona <xenona11x@gmail.com>
2024-08-05 23:58:18 +0300

    Fixed the size of the image fetched

--- src/constants/api.ts
+++ src/constants/api.ts
@@ -1,5 +1,5 @@
 export const ARTWORKS_ENDPOINT = "https://api.artic.edu/api/v1/artworks";
 export const IMAGE_LOWQ_ENDPOINT = (id: number | string) =>
-  `https://www.artic.edu/iiif/2/${id}/full/450,/0/default.jpg`;
+  `https://www.artic.edu/iiif/2/${id}/full/full/0/default.jpg`;
 export const IMAGE_HIGHQ_ENDPOINT = (id: number | string) =>
   `https://www.artic.edu/iiif/2/${id}/full/843,/0/default.jpg`;
