e1699908b7c477495a4d1985518017e0b0db51cb
Xenona <xenona11x@gmail.com>
2024-08-06 10:19:51 +0300

    Fixed short gallery style inconsistency

--- src/components/ShortGallery/styled.ts
+++ src/components/ShortGallery/styled.ts
@@ -1,8 +1,9 @@
 import styled from "styled-components";
 export const ShortGalleryContainer = styled.div`
   display: grid;
-  grid-template-columns: repeat(3, auto);
+  grid-template-columns: repeat(3, minmax(auto, 35%));
   gap: 16px;
+
   @media screen and (max-width: 1000px) {
     grid-template-columns: repeat(2, auto);
   }
