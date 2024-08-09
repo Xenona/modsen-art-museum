19c3ec7668c6c369a50c220f720652b08bcb90aa
Xenona <xenona11x@gmail.com>
2024-08-09 19:45:44 +0300

    Added more constants

--- src/constants/api.ts
+++ src/constants/api.ts
@@ -1,3 +1,19 @@
 export const ARTWORKS_ENDPOINT = "https://api.artic.edu/api/v1/artworks";
+
+export const ARTWORK_ID_ENDPOINT = (id: number) => `${ARTWORKS_ENDPOINT}/${id}`;
+
+export const ARTWORKS_PARAMETRIZED_ENDPOINT = ({
+  fields = "",
+  limit,
+  page = 1,
+}: Partial<{
+  fields: string;
+  limit: number;
+  page: number;
+}>) => `${ARTWORKS_ENDPOINT}?page=${page}&limit=${limit}$fields=${fields}`;
+
 export const IMAGE_ENDPOINT = (id: number | string) =>
   `https://www.artic.edu/iiif/2/${id}/full/full/0/default.jpg`;
+
+export const SEARCH_ENDPOINT = (size: number, q: string) =>
+  `${ARTWORKS_ENDPOINT}/search?q=${q}&size=${size}`;
